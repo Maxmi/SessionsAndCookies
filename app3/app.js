1c9bd8662f38073cacb7dd797d507e7bec4744a8
Gulmira Berdalieva <gulmira2410@gmail.com>
2017-10-14 17:03:43 -0700

    implemented app3

--- app3/app.js
+++ app3/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const onHeaders = require('on-headers');
 
 app.set('view engine', 'pug');
 
@@ -9,16 +10,22 @@ app.use(express.static('public'));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  req.session = req.cookies.sessionCookie;
+  onHeaders(res, () => {
+    res.cookie('sessionCookie', req.session);
+  });
+  next();
+});
+
 
 app.get('/', (req, res) => {
-  const userInfo = req.cookies.userInfo;
-  res.render('index', userInfo);
+  res.render('index', req.session);
 });
 
 app.post('/', (req, res) => {
-  const userInfo = req.body;
-  res.cookie('userInfo', userInfo);
-  res.render('index', userInfo);
+  req.session = req.body;
+  res.render('index', req.session);
 });
 
 app.listen(3000, () => {
